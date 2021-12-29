31169f107a0ac5b066d8a0e6fb644ef7396a5e0c
Ramón Escobar <ramon.escobarm97@gmail.com>
2021-12-28 20:29:57 -0600

    Al menos ya se ve la tabla

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 import pandas as pd
-import pandas_datareader.data as web
 import datetime
 from datetime import date
 import dash
-from dash import dcc, html
+from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output
-import time
+import crypto_yahoo as cy
+import dash_bootstrap_components as dbc
 
 
 sym = pd.read_csv('crypto-currencies.csv')
 
 # APP deployment
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
 
 app.layout = html.Div(
     children=[
-        html.H1(children='Hello Dash'),
 
-        html.Div(
-            id='filters',
-            children=[
+        # Storing the information
+        dcc.Store(id='store_info', storage_type='local'),
 
+        # Filter section
+        dbc.Row([
+            dbc.Col(html.Div([
                 dcc.DatePickerRange(
                     id='date-picker-range',
                     clearable=True,
@@ -30,21 +31,35 @@ app.layout = html.Div(
                     initial_visible_month=date.today() - datetime.timedelta(days=365),
                     start_date=date.today() - datetime.timedelta(days=365),
                     end_date=date.today()
-                ),
+                )
+            ]), width=3),
 
+            dbc.Col(html.Div([
                 dcc.RadioItems(
                     id='currency-radio-items',
                     options=[{'label': s, 'value': s} for s in ['USD', 'EUR', 'CAD', 'GBP']],
                     value='USD',
-                ),
+                )
+            ]), width=3),
 
+            dbc.Col(html.Div([
                 dcc.Dropdown(
                     id='stock-ticker-input',
-                    options=[{'label': s, 'value': s} for s in sym['symbol']],
-                    value='AAPL',
+                    options=[{'label': r, 'value': s} for r, s in zip(sym.name, sym.symbol)],
+                    value=["BTC"],
                     multi=True
                 )
+            ]), width=3)
+        ]),
 
+        # Graphic section
+
+        html.Div(
+            id='graphs',
+            children=[
+                dash_table.DataTable(id='table',
+                                     columns=[{"name": i, "id": i} for i in ['Date', 'Crypto', 'Price']]
+                                     )
             ]
         ),
 
@@ -53,6 +68,25 @@ app.layout = html.Div(
 )
 
 
+@app.callback(
+    Output('store_info', 'data'),
+    [
+        Input('stock-ticker-input', 'value'),
+        Input('currency-radio-items', 'value'),
+        Input('date-picker-range', 'start_date'),
+        Input('date-picker-range', 'end_date')
+    ]
+)
+def filter_table(ticker, currency, start_date, end_date):
+    df = cy.multiple_stocks(ticker, currency, start_date, end_date)
+    return df.to_dict('records')
+
+@app.callback(
+    Output('table', 'data'),
+    Input('store_info', 'data')
+)
+def update_table(data):
+    return data
 
 
 if __name__ == '__main__':
---
+++ crypto_yahoo.py
@@ -0,0 +1,23 @@
+import pandas_datareader.data as web
+import pandas as pd
+import time
+from functools import reduce
+
+
+def get_crypto(coin, start_date, end_date):
+    hist = web.DataReader(f"{coin}", "yahoo", start_date, end_date)
+    hist[f"{coin}"] = hist["Close"]
+    hist = hist[[f'{coin}']]
+    return hist
+
+def multiple_stocks(stocks, currency, start_date, end_date):
+    time.sleep(3)
+    dfs = []
+    for c in stocks:
+        yc = c + "-" + currency
+        dfs.append(get_crypto(yc, start_date, end_date))
+    dfm = reduce(lambda left, right: pd.merge(left, right, on=['Date'], how='outer'), dfs)
+    dfmu = dfm.reset_index().melt(id_vars=['Date'],
+                                  var_name='Crypto',
+                                  value_name='Price')
+    return dfmu
